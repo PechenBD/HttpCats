69974bdf58e6577c94e3c131f014cf37dded628d
Kirill Pechenkin <kirill-11@yandex.ru>
2023-06-24 23:14:30 +1000

    initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,37 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Post {
+    private String id;
+    private String text;
+    private String type;
+    private String user;
+    private int upvotes;
+
+    public Post(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") int upvotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Post:" +
+                "\n id=" + id +
+                "\n text=" + text +
+                "\n type=" + type +
+                "\n user=" + user +
+                "\n upvotes=" + upvotes;
+    }
+}
